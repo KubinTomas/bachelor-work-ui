8cb725395b928f7508591260cd6956a403f672fd
TomasKubin <Kubin.Tom@email.cz>
2021-02-20 21:33:25 +0100

    empty welcome page

--- src/app/pages/main/components/default/default.component.ts
+++ src/app/pages/main/components/default/default.component.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { NgxPermissionsService } from 'ngx-permissions';
 import { Subscription } from 'rxjs';
 import { UserModel } from 'src/app/core/models/authentication/user.model';
 import { roleAdmin, roleStudent } from 'src/app/core/models/constants';
 import { AuthenticationService } from 'src/app/core/services/authentication/authentication.service';
+import { NotificationToastrService } from 'src/app/core/services/notification/notification-toastr.service';
 import { AuthActions } from 'src/app/pages/authentication/store/auth-action-types';
 import { user } from 'src/app/pages/authentication/store/auth.selectors';
 import { AppState } from 'src/app/store/app.reducer';
@@ -29,7 +31,9 @@ export class DefaultComponent implements OnInit, OnDestroy {
   constructor(
     private authService: AuthenticationService,
     private store: Store<AppState>,
-    private permissionsService: NgxPermissionsService) {
+    private permissionsService: NgxPermissionsService,
+    private router: Router,
+    private notificationToastrService: NotificationToastrService) {
     this.store.dispatch(AuthActions.getUser());
   }
 
@@ -40,7 +44,7 @@ export class DefaultComponent implements OnInit, OnDestroy {
 
       if (this.user) {
         const roles = [this.user.activeStagUserInfo.role];
-        
+
         this.permissionsService.flushPermissions();
         this.permissionsService.loadPermissions(roles);
       }
@@ -61,6 +65,8 @@ export class DefaultComponent implements OnInit, OnDestroy {
 
   changeRole(userName: string): void {
     this.authService.changeRole(userName).subscribe(user => {
+      this.notificationToastrService.showSuccess('Role byla změněna');
+      this.router.navigateByUrl('');
       this.store.dispatch(AuthActions.saveUser({ user }));
     });
   }
--- src/app/pages/main/components/welcome-page/welcome-page.component.ts
+++ src/app/pages/main/components/welcome-page/welcome-page.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { constants } from 'buffer';
+import { NgxPermissionsService } from 'ngx-permissions';
+import { Subscription } from 'rxjs';
+import { roleAdmin, roleStudent } from 'src/app/core/models/constants';
+import { user } from 'src/app/pages/authentication/store/auth.selectors';
+import { AppState } from 'src/app/store/app.reducer';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
-export class WelcomePageComponent implements OnInit {
+export class WelcomePageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  subs: Subscription = new Subscription();
+
+  isAdmin = false;
+  isStudent = false;
+
+  constructor(
+    private store: Store<AppState>,
+    private permissionsService: NgxPermissionsService,
+    private router: Router) {
+
+  }
 
   ngOnInit(): void {
+    this.subs.add(this.store.select(user).subscribe(userInStore => {
+
+      if (userInStore) {
+        this.permissionsService.hasPermission(roleAdmin).then(res => {
+          this.isAdmin = res;
+        });
+
+        this.permissionsService.hasPermission(roleStudent).then(res => {
+          this.isStudent = res;
+        });
+      }
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 
 }
