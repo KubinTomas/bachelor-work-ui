7f834175e98142ddcf4b02bf9db54f9045bf4cd2
TomasKubin <Kubin.Tom@email.cz>
2021-03-12 14:30:03 +0100

    api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   stagLoginUrl: 'https://stag-demo.zcu.cz/ws/login?originalURL=',
-  stagSuccessRedirectToUrl: 'http://localhost:4200/login/stag/validation',
-  apiUrl : 'http://localhost:50466'
+  stagSuccessRedirectToUrl: 'https://shinyapps.ki.ujep.cz/restag/login/stag/validation',
+  apiUrl : 'https://shinyapps.ki.ujep.cz/restag/api'
 };
